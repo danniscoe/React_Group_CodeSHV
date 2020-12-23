9b86d46154533b1cf747875d277f65b41b1fd3b8
Danni St.Claire-Coe <47538480+danniscoe@users.noreply.github.com>
2020-12-23 13:34:39 -0600

    Add files via upload

--- src/components/DisplayWeather.js
+++ src/components/DisplayWeather.js
@@ -2,10 +2,8 @@ import React from "react";
 import "./displayweather.css";
 function DisplayWeather(props) {
   const { data } = props;
-  const iconurl =
-    "http://openweathermap.org/img/w/" +
-    `${data.cod != 404 ? data.weather[0].icon : null}` +
-    ".png";
+  const iconurl ="http://openweathermap.org/img/w/" +
+    `${data.cod != 404 ? data.weather[0].icon : null}` + ".png";
   return (
     <div className="displayweather">
       {data.cod != 404 ? (
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,13 +1,10 @@
 import React, { useState } from "react";
 import DisplayWeather from "./DisplayWeather";
 import "./weather.css";
-const APIKEY = "94782b26f0f7dd835a8faee04d659260";
-
 
 function refreshWeather() {
-    /* working out the details on when and where to call the 5 minute timer */
-      nIntervId = setInterval(Weather, 300000);
-    }
+  nIntervId = setInterval(Weather, 300000);
+}
 
 function Weather() {
   const [weather, setWeather] = useState([]);
@@ -16,12 +13,11 @@ function Weather() {
     country: "",
   });
 
-  // const APIKEY = process.env.apiKeyCall;
-  
+  const APIKEY = "38dcf0092ca63585bb828f52eb2ecc12";
   async function weatherData(e) {
     e.preventDefault();
     if (form.city == "") {
-      alert("Please give me something to work with");
+      alert("Add values");
     } else {
       const data = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${form.city},${form.country}&APPID=${APIKEY}`
@@ -36,9 +32,8 @@ function Weather() {
   const handleChange = (e) => {
     let name = e.target.name;
     let value = e.target.value;
-    
-    /* removing special char excluding the dash */
-    value = e.target.value.replace(/[/0123456789!$%^&*()_+|~=`{}[:;<>?,.@#\]]/g, "");
+
+    value = e.target.value.replace(/[!$%^&*()_+|~=`{}[:;<>?,.@#\]]/g, "");
     e.target.value = value;
 
     if (name == "city") {
@@ -50,12 +45,14 @@ function Weather() {
   };
   return (
     <div className="weather">
-      <span className="title">Weather App</span>
+      <span className="title">Channel Two Weather</span>
+    
       <br />
+      
       <form>
         <input
           type="text"
-          placeholder="city"
+          placeholder="City"
           name="city"
           onChange={(e) => handleChange(e)}
         />
@@ -67,10 +64,10 @@ function Weather() {
           onChange={(e) => handleChange(e)}
         />
         <button className="getweather" onClick={(e) => weatherData(e)}>
-          Submit
+          Here comes the sun!
         </button>
       </form>
-
+      
       {/* {console.log(weather)} */}
       {weather.data != undefined ? (
         <div>
@@ -78,6 +75,7 @@ function Weather() {
         </div>
       ) : null}
     </div>
+    
   );
 }
 
